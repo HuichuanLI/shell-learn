74c73fa51d1c4288c87582b54d0b661ff8001f61
huichuan <lhc14124908@163.com>
2020-01-11 17:04:32 +0800

    add

---
+++ shell -learn/CH1shell基础/case.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+case $1 in 
+	jack)
+		echo "Hello jack"
+		;;
+	mike)
+		echo "mike"
+		;;
+	*)
+		echo "everyone"
+		;;
+esac
---
+++ shell -learn/CH1shell基础/double_square_bracket.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+if [[ $2 -gt $1 &&  $3 -gt $2 ]];then
+	echo "yes"
+else
+	echo "no"
+fi
+
