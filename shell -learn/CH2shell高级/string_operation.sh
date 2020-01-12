7351b8618300792cae89ad6b8380e1737e7de0eb
huichuan <lhc14124908@163.com>
2020-01-12 20:04:46 +0800

    add

--- shell -learn/CH2shell高级/string_operation.sh
+++ shell -learn/CH2shell高级/string_operation.sh
@@ -3,7 +3,27 @@
 
 var1="Hello world"
 echo ${#var1}
-
+echo `expr length "$var1"`
 #获取索引位置
 var2="quickstart is a app"
-$(( index "$var1" start))
+echo `expr index "$var2" start`
+echo `expr index "$var2" uniq`
+#获取字段长度
+var3="quickstart is a app"
+#从头开始的
+sublen=`expr match "$var2" quick`
+echo $sublen
+#抽取字符串的字串
+var1="kafka hadoop yarn mapreduce"
+#从0开始从起始位置开始查找
+substr=${var1:10}
+echo $substr
+
+echo ${var1:10:5}
+#要有空格
+echo ${var1: -5}
+#另一种形式
+echo ${var1:(-5):2}
+#从1开始
+substrsubstr=`expr substr "$var1" 10 5`
+echo $substrsubstr
