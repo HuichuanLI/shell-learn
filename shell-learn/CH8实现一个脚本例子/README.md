4cdd5f0eeb56d24e8a5559df2590c923a2eafde4
huichuan <lhc14124908@163.com>
2020-01-21 10:18:15 +0800

    add

--- shell-learn/CH8实现一个脚本例子/README.md
+++ shell-learn/CH8实现一个脚本例子/README.md
@@ -18,17 +18,6 @@
 			2、停止服务器进程脚本app_stop.sh
 			拆分脚本功能，抽象函数
 	
-		1、function get_all_process		返回进程名称列表字符串
-		
-		2、function get_all_group		返回进程组列表字符串
-		
-		3、function get_process_info	返回进程详细信息列表字符串，详细信息包括：运行状态、PID、CPU、MEM、启动时间
-		
-			注：该函数可以接收一个参数，参数为进程名称
-			
-		4、function get_all_process_by_group	返回进程组内的所有进程名称列表字符串
-			
-			例子：DB组--> "mysql postgresql oracle"
 		 
 		功能函数一代码：
             function get_all_group
@@ -40,4 +29,39 @@
 		1	无参数			列出配置文件中所有进程的运行信息
 		2	-g GroupName	列出GroupName组内的所有进程
 		3	process_name1	列出指定进程的运行信息
+
+
+    function get_all_group
+	
+		说明：该函数无需输入任何参数；返回配置文件 process.cfg中所有的组信息，例如WEB、DB等
+	
+	function get_all_process
+	
+		说明：该函数无需输入任何参数；返回配置文件 process.cfg中所有的进程信息。
+	
+	function get_process_pid_by_name
+	
+		说明：该函数接收一个参数，参数为进程名称；返回值是一个PID的列表，可能有一个PID，也可能有多个
+	
+	function get_process_info_by_pid
+	
+		说明：该函数接收一个参数，参数为进程PID；返回值是一个进程运行信息的列表，列表包含运行状态、CPU占用率、内存占用率、进程启动时间
+	
+	function is_group_in_config
+	
+		说明：该函数接收一个参数，参数为组的名称；返回值是0或1,0代表该组在配置文件中，1代表该组不在配置文件中
+	
+	function get_all_process_by_group
+	
+		说明：该函数接收一个参数，参数为组名称；返回值是对应组内的所有进程名称列表
 		
+	function get_group_by_process_name
+	
+		 说明：该函数接收一个参数，参数是一个进程名称；返回值是一个组名
+    function format_print	 
+	
+		说明：该函数接收二个参数，第一个参数为process_name，第二个参数为组名称
+		返回值，是针对每一个进程PID的运行信息
+    function is_process_in_config
+
+    说明：该函数接收一个参数，参数为进程名称；返回值是0或1,0代表该进程在配置文件中，1代表进程不在配置文件中
--- shell-learn/CH8实现一个脚本例子/app_status.sh
+++ shell-learn/CH8实现一个脚本例子/app_status.sh
@@ -55,6 +55,18 @@ function get_process_info_by_pid
 
 }
 
+function is_process_in_config
+{
+	for pn in `get_all_process`;do
+		if [ $pn == $1 ];then
+			return
+		fi
+	done
+	echo "Process $1 is not in process.cfg"
+	return 1
+}
+
+
 function is_group_in_config
 {
 	for gn in `get_all_group`;do
@@ -77,25 +89,72 @@ function get_group_by_process_name
 }
 
 
-f [ $# -gt 0 ];then
-	if [ "$1" == "-g" ];then
+function get_all_process_by_group
+{
+	is_group_in_config $1
+	if [ $? -eq 0 ];then
+		p_list=`sed -n "/\[$1\]/,/\[.*\]/p" $HOME_DIR/$CONFIG_FILE | egrep -v "(^$|^#|\[.*\])"`
+		echo $p_list
+	else
+		 echo "GroupName $1 is not in process.cfg"
+	fi
+}
+
+
+function get_group_by_process_name
+{
+	for gn in `get_all_group`;do
+		for pn in `get_all_process_by_group $gn`;do
+			if [ $pn == $1 ];then
+				echo "$gn"
+			fi
+		done
+	done
+}
+
+function format_print
+{
+    ps -ef | grep $1 | grep -v grep | grep -v $this_pid &> /dev/null
+    if [ $? -eq 0 ];then
+        pids=`get_process_pid_by_name $1`
+        for pid in $pids;do
+          get_process_info_by_pid $pid
+          awk -v p_name=$1 \
+            -v g_name=$2 \
+            -v p_status=$pro_status \
+            -v p_pid=$pid	\
+            -v p_cpu=$pro_cpu \
+            -v p_mem=$pro_mem \
+            -v p_start_time="$pro_start_time" \
+            'BEGIN{printf "%-20s%-12s%-10s%-6s%-7s%-10s%-20s\n",p_name,g_name,p_status,p_pid,p_cpu,p_mem,p_start_time}'
+        done
+    else
+        awk -v p_name=$1 -v g_name=$2 'BEGIN{printf "%-20s%-12s%-10s%-6s%-7s%-10s%-20s\n",p_name,g_name,"STOPPED","NULL","NULL","NULL","NULL"}'
+    fi
+}
+
+awk 'BEGIN{printf "%-20s%-10s%-10s%-6s%-7s%-10s%-20s\n","ProcessName---------","GroupName---","Status----","PID---","CPU----","MEMORY----","StartTime---"}'
+
+
+#get_group_by_process_name mysql
+if [ $# -gt 0 ];then
+	if [ "$1" = "-g" ];then
 		shift
-		for gn in $@;do
-			is_group_in_config $gn || continue
-			for pn in `get_all_process_by_group $gn`;do
+	  for gn in $@;do
+      for pn in `get_all_process_by_group $gn`;do
 				is_process_in_config $pn && format_print $pn $gn
 			done
-		done
+	  done
 	else
-		for pn in $@;do
+     for pn in $@;do
 			gn=`get_group_by_process_name $pn`
 			is_process_in_config $pn && format_print $pn $gn
 		done
-	fi
+  fi
 else
-	for pn in `get_all_process`;do
-		gn=`get_group_by_process_name $pn`
-                is_process_in_config $pn && format_print $pn $gn
+  for pn in `get_all_process`;do
+		  gn=`get_group_by_process_name $pn`
+      is_process_in_config $pn && format_print $pn $gn
 	done
 fi
 
